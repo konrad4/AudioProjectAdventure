115e9d78b6c50f022cb1878303bc06790611f44c
Konrad <15710231+konrad4@users.noreply.github.com>
2021-02-12 02:41:30 +0100

    Add collider to player

---
+++ AudioProjectAdventure/Assets/Scripts/MonoBehaviours/Player/PlayerAudio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerAudio : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent agent;
+
+    // Update is called once per frame
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Debug.Log(other);
+    }
+}
