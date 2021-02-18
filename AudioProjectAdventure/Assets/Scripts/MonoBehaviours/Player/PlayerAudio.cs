de295dcf404ae9bd8be7144b69f6f597a129fd67
Konrad <15710231+konrad4@users.noreply.github.com>
2021-02-18 01:04:14 +0100

    Add walk sound and logic

--- AudioProjectAdventure/Assets/Scripts/MonoBehaviours/Player/PlayerAudio.cs
+++ AudioProjectAdventure/Assets/Scripts/MonoBehaviours/Player/PlayerAudio.cs
@@ -7,11 +7,62 @@ using UnityEngine.AI;
 public class PlayerAudio : MonoBehaviour
 {
     [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private AudioClip metalWalkClip;
+    [SerializeField] private AudioClip carpetWalkClip;
+    [SerializeField] private AudioSource footstepsSource;
+    private string floorType;
+    List <GameObject> currentCollisions = new List <GameObject> ();
+    public bool switchedOn;
+    
+    private void Update()
+    {
+        if (!agent.isStopped)
+        {
+            if (floorType == "Metal")
+            {
+                PlayAudio(metalWalkClip);
+            }
+            else
+            {
+                Debug.Log("DU");
+                PlayAudio(carpetWalkClip);
+            }    
+        }
+        else StopAudio();
+    }
+    
+    private void PlayAudio(AudioClip clipToPlay)
+    {
+        if (!footstepsSource.isPlaying)
+        {
+            footstepsSource.clip = clipToPlay;
+            footstepsSource.Play(); 
+        }
 
-    // Update is called once per frame
+    }
 
+    private void StopAudio()
+    {
+        if (footstepsSource.isPlaying)
+        {
+            footstepsSource.Stop();
+        } 
+
+    }
+
+    private void SetFloorType(string typeOfFloor)
+    {
+        floorType = typeOfFloor;
+    }
+    
     private void OnCollisionEnter(Collision other)
     {
-        Debug.Log(other);
+        currentCollisions.Add (other.gameObject);
+
+        foreach (GameObject gObject in currentCollisions)
+        {
+            if (gObject.CompareTag("Carpet")) SetFloorType("Carpet");
+            else SetFloorType("Metal");
+        }
     }
 }
