1225fece95499d540ceb10fda5a171b03571f176
Konrad <15710231+konrad4@users.noreply.github.com>
2021-02-19 02:57:42 +0100

    Finish Market scene

--- AudioProjectAdventure/Assets/Scripts/MonoBehaviours/Player/PlayerAudio.cs
+++ AudioProjectAdventure/Assets/Scripts/MonoBehaviours/Player/PlayerAudio.cs
@@ -7,25 +7,11 @@ public class PlayerAudio : MonoBehaviour
     [SerializeField] private AudioClip[] metalWalkClips;
     [SerializeField] private AudioClip[] sandWalkClips;
     [SerializeField] private AudioSource footstepsSource;
-    [SerializeField] private GameObject secondHandGameObject;
-    [SerializeField] private GameObject playerGameObject;
     private Scene scene;
 
     private void Start()
     {
         scene = SceneManager.GetActiveScene();
-    }
-
-    private void Update()
-    {
-        if ((secondHandGameObject.transform.position - playerGameObject.gameObject.transform.position).sqrMagnitude < 50f)
-        {
-            Debug.Log("jest");
-        }
-        else
-        {
-            Debug.Log("nie ma");
-        }
     }
 
     private void PlayRandomSound(AudioClip[] clips)
---
+++ AudioProjectAdventure/Assets/ZoneSnapshotControl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class ZoneSnapshotControl : MonoBehaviour
+{
+    [SerializeField] private GameObject zoneBorderGameObject;
+    [SerializeField] private GameObject playerGameObject;
+    [SerializeField] private AudioMixerSnapshot zone1Snapshot;
+    [SerializeField] private AudioMixerSnapshot zone2Snapshot;
+
+    private const float transitionDuration = 2f;
+    
+    private void Update()
+    {
+        if ((zoneBorderGameObject.transform.position - playerGameObject.gameObject.transform.position).sqrMagnitude < 50f)
+        {
+            zone2Snapshot.TransitionTo(transitionDuration);
+        }
+        else
+        {
+            zone1Snapshot.TransitionTo(transitionDuration);
+        }
+    }
+}
